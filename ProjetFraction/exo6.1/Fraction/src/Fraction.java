baabf7b5c8b7307da7cdbd7c85939211a244400d
Moussaab <moussaab.el-ouali@ens.uvsq.fr>
2019-10-17 10:39:07 +0200

    second commit

---
+++ ProjetFraction/exo6.1/Fraction/src/Fraction.java
@@ -0,0 +1,23 @@
+
+public class Fraction {
+
+	private int numerator, denominator;
+	
+	 public Fraction(){
+	      numerator = denominator = 0;
+	 }
+	 public int getNumerator(){
+	      return numerator;
+	 }
+	 public void setNumerator(int num){
+	      numerator=num;
+	 }
+	 public int getDenominator(){
+	      return denominator;
+	 }
+	 public void setDenominator(int den){
+	      denominator=den;
+	 }
+	 
+	 
+}
